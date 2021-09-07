38a02d7eeff1bdfd0a8c6c64790a1761f1f7213b
nckim <qazqaz1000@hanmail.net>
2021-09-07 22:48:57 +0900

    diskController

--- heap_diskController.java
+++ heap_diskController.java
@@ -1,17 +1,31 @@
 import java.util.PriorityQueue;
 import java.util.*;
 class DiskController {
+
     public int solution(int[][] jobs) {
         int answer = 0;
+        Arrays.sort(jobs, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+
+
         PriorityQueue<Job> pq = new PriorityQueue<>();
-        for(int i = 0; i < jobs.length ; i++){
-            Job job = new Job(jobs[i][0], jobs[i][1]);
-            pq.add(job);
-        }
-        int size = pq.size();
         int accumulation = 0;
+        int jobIndex = 0;
+        int count = 0;
         int avarage = 0;
-        while(!pq.isEmpty()){
+        while(count < jobs.length){
+            while(jobIndex < jobs.length && jobs[jobIndex][0] <= accumulation){
+                Job job = new Job(jobs[jobIndex][0], jobs[jobIndex++][1]);
+                pq.add(job);
+            }
+            if(pq.isEmpty()){
+                accumulation = jobs[jobIndex][0];
+            }
             Job curJob = pq.poll();
             if(accumulation == 0) {
                 avarage += curJob.mTime;
@@ -19,8 +33,9 @@ class DiskController {
                 avarage +=  accumulation + curJob.mTime - curJob.mStart;
             }
             accumulation += curJob.mTime;
+            count++;
         }
-        answer = avarage / size;
+        answer = avarage / jobs.length;
         System.out.println(answer);
         return answer;
     }
@@ -36,6 +51,5 @@ class DiskController {
         public int compareTo(Job o) {
             return this.mTime - o.mTime;
         }
-   
     }
 }
