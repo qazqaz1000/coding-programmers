cf3602a188b4a497a70b5fd9f42ff6e496f25c64
qazqaz1000 <qazqaz1000@hanmail.net>
2022-02-15 00:31:41 +0900

    carpet

---
+++ search_carpet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Carpet {
+
+
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        int max = brown + yellow;
+        
+        return answer;
+    }
+}
